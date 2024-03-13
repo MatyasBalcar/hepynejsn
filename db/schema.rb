193a4c242e9509d6fcedbaf32b17eb95576c16ae
Kamil Mensik <kamil.mensik@juicymo.cz>
2024-03-13 12:17:46 +0100

    Kamiluv commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,33 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_03_13_095102) do
+  create_table "cards", force: :cascade do |t|
+    t.integer "card_suit", default: 0, null: false
+    t.integer "card_value", default: 0, null: false
+    t.integer "position", default: 0, null: false
+    t.integer "deck_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["deck_id"], name: "index_cards_on_deck_id"
+  end
+
+  create_table "decks", force: :cascade do |t|
+    t.integer "game_id", null: false
+    t.integer "current_card", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_decks_on_game_id"
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "wager"
+    t.boolean "result"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_games_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
